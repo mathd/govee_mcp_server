38862381c3392004a218773b54b4944b6d56aba4
Mathieu Dupuis <mathieu.dupuis@moov.ai>
2025-03-07 13:49:10 -0500

    Update .gitignore and modify inspector scripts to use current directory

--- inspector.sh
+++ inspector.sh
@@ -1,1 +1,1 @@
-npx @modelcontextprotocol/inspector uv --directory ˜/Sources/govee_mcp_server run python src/govee_mcp_server/server.py
+npx @modelcontextprotocol/inspector uv --directory . run python src/govee_mcp_server/server.py
